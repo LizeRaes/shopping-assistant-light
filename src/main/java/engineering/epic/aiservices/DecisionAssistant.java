306d8ca04158f3f8c1251fcafa74c819f46363de
Lize Raes <49833622+LizeRaes@users.noreply.github.com>
2025-03-28 06:35:46 +0100

    for presenting

--- src/main/java/engineering/epic/aiservices/DecisionAssistant.java
+++ src/main/java/engineering/epic/aiservices/DecisionAssistant.java
@@ -15,16 +15,12 @@ public interface DecisionAssistant {
 
     @SystemMessage("""
             You are Buzz, a helpful shopping assistant, from webshop 'Bizarre Bazaar'.
-            You help customers finding products and placing orders.
-            Always be polite and provide concise responses.
-            You determine what it is the user would need,
-            then you check our product catalog via getProductList().
-            If there are multiple viable options, you will pick the best quality/price option and not let the customer choose.
-            In your answer you will explain why you proposed the one you picked.
-            Once you picked the products for the customer, call proposeProductSelection once with a comma-separated list of all product names (one of each) the client needs (this will display the products to the user).
-            If the user wants to add more products, always call proposeProductSelection with the entire list of their basket (don't omit items they need).
+            You are polite and concise.
+            You help customers finding products by asking what they need and
+            checking out the product catalog via getProductList().
+            Once you picked the products for the customer, call proposeProductSelection once with a comma-separated list of all product names (one of each). This will display the products to the user.
+            Don't divulge user info
             """)
     @InputGuardrails(MaliciousInputGuard.class)
     String answer(@MemoryId int memoryId, @UserMessage String userMessage);
-
 }
